b8fb7b6317fbadd4e390b2395d84d779b8ecc4e8
Mickey <micksheridan.24@gmail.com>
2022-07-03 23:06:27 -0400

    Education and Creative Project updates

--- src/components/FinanceDashboard/components/Budget.tsx
+++ src/components/FinanceDashboard/components/Budget.tsx
@@ -2,18 +2,21 @@ import { IBudget } from "../../../types/FinanceTypes";
 import HealthBar from "../../Shared/HealthBar";
 import { deleteBudget } from "../FinanceService";
 
-export default function Budget (props: {budget: IBudget}){
-    const {budget}  = props 
+export default function Budget (props: {budget: IBudget, selectBudget: (id: IBudget) => void}){
+    const {budget, selectBudget}  = props 
 
-    return <div className="finance-item-container">
+    return <div onClick={() => selectBudget(budget)} className="finance-item-container">
         <div className="info">
             <h4 className="name">{budget.name} - ${budget.amount}</h4>
-            <div>${budget.spent!} - ${budget.amount - budget.spent!}</div>
+            <div>${budget.spent!} / ${budget.amount - budget.spent!}</div>
             <HealthBar amount={budget.spent!} total={budget.amount} redOnLow={false}></HealthBar>
+            {budget.fundName ? <div>Excess Funds to {budget.fundName}</div> : <></>}
         </div>
         <div className="tools">
-            <div className="delete" onClick={() => deleteBudget(budget.id!)}>X</div>
+            <div className="delete" onClick={(e) => {
+                e.stopPropagation()
+                deleteBudget(budget.id!)}}>X
+            </div>
         </div>
-    
     </div>
 }
